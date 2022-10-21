73bc2e7f0acfd34c3554ae9b8c5877486c96a75c
walissonsou <walissonsouza96@hotmail.com>
2022-10-21 17:22:04 -0300

    FUNDAMENTOS - CURSO FIAP

--- MANIPULACAO/List.py
+++ MANIPULACAO/List.py
@@ -6,3 +6,6 @@ while resposta == "S":
     inventario.append(int(input("Numero serial: ")))
     inventario.append(input("Departamento: "))
     resposta=input("Digite \"S\" para continuar: ").upper()
+
+    for itens in inventario:
+        print(itens)
