fd65d1485170f36cbb2748f5dd69217ad84af619
walissonsou <walissonsouza96@hotmail.com>
2022-10-21 17:33:45 -0300

    FUNDAMENTOS - CURSO FIAP

---
+++ MANIPULACAO/Indices.py
@@ -0,0 +1,19 @@
+valores=[]
+equipamentos=[]
+seriais=[]
+departamentos=[]
+resposta="S"
+while resposta == "S":
+    equipamentos.append((input("Equipamento: ")))
+    valores.append(float(input("Valor: ")))
+    seriais.append(int(input("Numero serial: ")))
+    departamentos.append(input("Departamento: "))
+    resposta=input("Digite \"S\" para continuar e aperte ENTER para imprimir os resultados: ").upper()
+
+for indice in range (0,len(equipamentos)):
+    print("\nEquipamento...: ",(indice+1))
+    print("Equipamento...: ", equipamentos[indice])
+    print("valores...: ",valores[indice])
+    print("seriais...: ",seriais[indice])
+    print("departamentos...: ",departamentos[indice])
+
