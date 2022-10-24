5fd6b437c6c58e96724316235c1ff7ae6a25889a
walissonsou <walissonsouza96@hotmail.com>
2022-10-23 23:56:43 -0300

    FUNDAMENTOS - CURSO FIAP

---
+++ LISTA/Dicionario.py
@@ -0,0 +1,11 @@
+usuarios = {}
+print(usuarios)
+
+usuarios = {
+    "chaves": ["Chaves", "2022-10-23", "TI"],
+    "quico": [ "Quico", "2022-10-24", "MANUTENÇÃO"]}
+
+print(usuarios)
+usuarios["Florinda"] = ["Dona Florinda", "2022-10-23", "MKT"]
+
+usuarios.get("quico")
