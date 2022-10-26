87a9fb02c4f8465788ac27d0e2c2cfaaa68eb5eb
walissonsou <walissonsouza96@hotmail.com>
2022-10-25 23:40:26 -0300

    FUNDAMENTOS - CURSO FIAP

---
+++ LISTA/Funcoes.py
@@ -0,0 +1,11 @@
+def perguntar():
+   return input("O que deseja realizar?\n" +
+                   "<I> - Para Inserir um usuário\n" +
+                   "<P> - Para Pesquisar um usuário\n" +
+                   "<E> - Para Excluir um usuário\n" +
+                   "<L> - Para Listar um usuário\n" +
+                   "<ENTER> - Para sair pressione enter\n").upper()
+def inserir(dicionario):
+   dicionario[input("Digite o login: ").upper()] = [input("Digite o nome").upper(),
+                                                      input("Digite a ultima data de acesso"),
+                                                      input("Qual a última estação acessada: ").upper()]
