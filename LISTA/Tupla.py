a1f54e268215bb61b3cc960ccfab4d86ebf951c9
walissonsou <walissonsouza96@hotmail.com>
2022-10-27 11:18:09 -0300

    Trabalhando com arquivos - MeuPrimeiroArquivo.py

--- LISTA/Tupla.py
+++ LISTA/Tupla.py
@@ -7,9 +7,10 @@ print(tupla)
 
 for chave in range(0,len(tupla)):
     print("Email: ", tupla[chave][1])
+  # dicionario com a chave da tupla.
     usuarios[tupla[chave]]=[input("Digite o nome"), input("Digite o nível")]
 
-for chave, valor in usuarios.items():
+for (chave, valor) in usuarios.items():
     print("Usuário...:", chave[0])
     print("Email...:", chave[1])
     print("Nome...:", chave[0])
---
+++ MANIPULAÇAO_ARQUIVOS/MeuPrimeiroArquivo.py
@@ -0,0 +1,5 @@
+with open("primeiro_arquivo.txt", "r") as arquivo:
+    for linha in arquivo.readlines():
+        print(linha)
+
+# A, W, X, R, WR
