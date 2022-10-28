36926f259523cadb0793db742f8068249f533036
walissonsou <walissonsouza96@hotmail.com>
2022-10-28 00:43:59 -0300

    Trabalhando com listas

---
+++ MANIPULAÇAO_ARQUIVOS/EncontrarEConquistar.py
@@ -0,0 +1,6 @@
+texto = "Eu amo python! "
+
+print(texto.find("o"))
+
+print(texto[texto.find("o")+1:].find("o"))
+
---
+++ MANIPULAÇAO_ARQUIVOS/Textos.py
@@ -0,0 +1,14 @@
+texto = "Leia e Han Solo"
+        #012345678901234
+
+print(texto[0:4])
+print(texto[7:])
+print(texto[-8:])
+
+# Inventer string
+print(texto[::-1])
+
+# 0: -> posição iniciao
+# 4: -> qntdd de informação
+# 2: -> o passo
+print(texto[0:4:2])
