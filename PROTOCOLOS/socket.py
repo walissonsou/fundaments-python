78733d0a065a270d9a212c4a895af0a7eb7f160c
walissonsou <walissonsouza96@hotmail.com>
2022-10-30 23:38:58 -0300

    Alteração no code

---
+++ PROTOCOLOS/socket.py
@@ -0,0 +1,27 @@
+from socket import *
+
+from _socket import socket
+
+from socket import *
+
+servidor="127.0.0.1"
+porta=43210
+
+obj_socket = socket(AF_INET, SOCK_STREAM)
+obj_socket.bind((servidor.porta))
+obj_socket.listen(2)
+
+print("Aguardando cliente...")
+
+
+while True:
+    con, cliente = obj_socket.accept()
+    print("Conectado com: ", cliente)
+    while True:
+        msg_recebida = str(con.recv(1024))
+        print("Recebemos: ", msg_recebida)
+        msg_enviada = 'Olá cliente'
+        con.send(msg_enviada)
+        break
+    con.close
+
