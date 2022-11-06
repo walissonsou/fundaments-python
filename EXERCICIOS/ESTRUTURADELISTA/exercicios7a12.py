a4c6b8d7b1e3fbaf7bf59b013d699da1292d187e
walissonsou <walissonsouza96@hotmail.com>
2022-11-06 17:28:10 -0300

    Resolução de exercícios PYTHONBRASIL

--- EXERCICIOS/ESTRUTURADELISTA/exercicios7a12.py
+++ EXERCICIOS/ESTRUTURADELISTA/exercicios7a12.py
@@ -64,19 +64,77 @@ print(listaaltura[::-1])
 
 print("fim ")
 
+
 # 9 Faça um Programa que leia um vetor A com 10 números inteiros, calcule e
 # mostre a soma dos quadrados dos elementos do vetor.
+
 numeros = []
+# Onde está no loop
+i = 0
 soma = 0
-for numero in range(10):
-    numeros.append(int(input(f"Digite um numero {numero+1}: ")))
-    soma = soma + (numeros[len(numeros)-1]**2)
-print("A soma do qdd dos numeros é {}".format(soma))
+# Loop apenas para voltar pro começo caso um número esteja inválido
+while len(numeros) != 10:
+    i += 1
+    print("Diga o número " + str(i) + "º:")
+    try:
+        numero = int(input())
+    except:
+        print("Número inválido.")
+        i -= 1
+        continue
+    numeros.append(numero)
+for numero in numeros:
+    soma = soma + (numero[len(numeros)-1]**2)
+    print(soma)
 
 
 
+# 10 Faça um Programa que leia dois vetores com 10 elementos cada. Gere um
+# terceiro vetor de 20 elementos, cujos valores deverão ser compostos pelos elementos
+# intercalados dos dois outros vetores.
+vetorA = []
+vetorB = []
+vetorC = []
+i = 0
+while len(vetorA) != 5:
+    i += 1
+    print("Diga o número " + str(i) + "º:")
+    try:
+        numero = int(input())
+    except:
+        print("Número inválido.")
+        i -= 1
+        continue
+    vetorA.append('y' + str(numero))
+i = 0
+# Loop apenas para voltar pro começo caso um número esteja inválido
+while len(vetorB) != 5:
+    i += 1
+    print("Diga o número " + str(i) + "º:")
+    try:
+        numero = int(input())
+    except:
+        print("Número inválido.")
+        i -= 1
+        continue
+    vetorB.append('x' + str(numero))
+print(vetorA)
+print(vetorB)
 
+# sem intercalar
+lista3 = vetorA + vetorB
+vetorC.append(lista3)
+print(vetorC)
+# com intercalação
+for elemento in range(5):
+    vetorC.append(vetorA[elemento])
+    vetorC.append(vetorB[elemento])
 
+print(vetorC)
 #  11 Altere o programa anterior, intercalando 3 vetores de 10 elementos cada.
+
+
+
+
 # 12 Foram anotadas as idades e alturas de 30 alunos. Faça um Programa que determine
 # quantos alunos com mais de 13 anos possuem altura inferior à média de altura desses alunos.
