3e59e9f58aafa43c53fc65f6f69e319e8d6c16e2
walissonsou <walissonsouza96@hotmail.com>
2022-11-06 17:18:39 -0300

    Resolução de exercícios PYTHONBRASIL

--- EXERCICIOS/ESTRUTURADELISTA/exercicios7a12.py
+++ EXERCICIOS/ESTRUTURADELISTA/exercicios7a12.py
@@ -64,11 +64,19 @@ print(listaaltura[::-1])
 
 print("fim ")
 
-
 # 9 Faça um Programa que leia um vetor A com 10 números inteiros, calcule e
 # mostre a soma dos quadrados dos elementos do vetor.
-# 10 Faça um Programa que leia dois vetores com 10 elementos cada. Gere um
-# terceiro vetor de 20 elementos, cujos valores deverão ser compostos pelos elementos intercalados dos dois outros vetores.
+numeros = []
+soma = 0
+for numero in range(10):
+    numeros.append(int(input(f"Digite um numero {numero+1}: ")))
+    soma = soma + (numeros[len(numeros)-1]**2)
+print("A soma do qdd dos numeros é {}".format(soma))
+
+
+
+
+
 #  11 Altere o programa anterior, intercalando 3 vetores de 10 elementos cada.
 # 12 Foram anotadas as idades e alturas de 30 alunos. Faça um Programa que determine
 # quantos alunos com mais de 13 anos possuem altura inferior à média de altura desses alunos.
