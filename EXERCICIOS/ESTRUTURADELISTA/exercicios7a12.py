6316becd720ea6c9f6af99e051944850a597bf7d
walissonsou <walissonsouza96@hotmail.com>
2022-11-06 21:55:10 -0300

    Resolução de exercícios PYTHONBRASIL

--- EXERCICIOS/ESTRUTURADELISTA/exercicios7a12.py
+++ EXERCICIOS/ESTRUTURADELISTA/exercicios7a12.py
@@ -131,10 +131,71 @@ for elemento in range(5):
     vetorC.append(vetorB[elemento])
 
 print(vetorC)
+vetorA = []
+vetorB = []
+vetorC = []
+vetorD = []
+i = 0
+while len(vetorA) != 5:
+    i += 1
+    print("Diga o número " + str(i) + "º:")
+    try:
+        numero = int(input())
+    except:
+        print("Número inválido.")
+        i -= 1
+        continue
+    vetorA.append('y' + str(numero))
+i = 0
+# Loop apenas para voltar pro começo caso um número esteja inválido
+while len(vetorB) != 5:
+    i += 1
+    print("Diga o número " + str(i) + "º:")
+    try:
+        numero = int(input())
+    except:
+        print("Número inválido.")
+        i -= 1
+        continue
+    vetorB.append('x' + str(numero))
+
+i = 0
+# Loop apenas para voltar pro começo caso um número esteja inválido
+while len(vetorC) != 5:
+    i += 1
+    print("Diga o número " + str(i) + "º:")
+    try:
+        numero = int(input())
+    except:
+        print("Número inválido.")
+        i -= 1
+        continue
+    vetorC.append('x' + str(numero))
 #  11 Altere o programa anterior, intercalando 3 vetores de 10 elementos cada.
 
+for elemento in range(5):
+    vetorD.append(vetorA[elemento])
+    vetorD.append(vetorB[elemento])
+    vetorD.append(vetorC[elemento])
+print(vetorD)
+
+# 12 Foram anotadas as idades e alturas de 30 alunos.
+# Faça um Programa que determine quantos alunos com mais
+# de 13 anos possuem altura inferior à média de altura desses alunos.
+
+dados = [{'Nome': 'walisson', 'idade': 15, 'altura': 1.65},{'Nome': 'Souza', 'idade': 12, 'altura': 1.70}]
+alturas = []
+idades = []
+
+for tamanho in dados:
+    idades.append(tamanho['idade'])
+    alturas.append(tamanho['altura'])
+    media = sum(alturas) / len(alturas)
+quantidadedeAlunos = 0
+
+for i in range(len(idades)):
+    if(idades[i]) > 13 and alturas[i] < media:
+        quantidadedeAlunos += 1
 
 
-
-# 12 Foram anotadas as idades e alturas de 30 alunos. Faça um Programa que determine
-# quantos alunos com mais de 13 anos possuem altura inferior à média de altura desses alunos.
+print("A quantidade de alunos que tem + 13 anos é {}" .format(quantidadedeAlunos))
