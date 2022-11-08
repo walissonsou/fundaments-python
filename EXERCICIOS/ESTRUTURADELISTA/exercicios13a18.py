68283d9bfc334a58f0e12f36786f7c346434f21d
walissonsou <walissonsouza96@hotmail.com>
2022-11-07 23:58:12 -0300

    Resolução de exercícios PYTHONBRASIL

--- EXERCICIOS/ESTRUTURADELISTA/exercicios13a18.py
+++ EXERCICIOS/ESTRUTURADELISTA/exercicios13a18.py
@@ -3,3 +3,28 @@
 # temperaturas acima da média anual,# e em que mês elas ocorreram
 # (mostrar o mês por extenso: 1 – Janeiro, 2 – Fevereiro, . . . ).
 
+listadetemperatura = [{'JAN': 25 , 'FEV': 30, 'MAR': 12, 'ABRIL': 1.70}]
+temperatura = []
+lugar = []
+def cadastrar_temperaturas(jan,fev,mar,abr,maio,jun):
+    cadastro = {'lugar': lugar, "jan": jan, "fev": fev, "mar": mar, 'abr': abr, "maio": maio, "jun": jun}
+    temperatura.append(cadastro['jan'])
+    temperatura.append(cadastro['fev'])
+    temperatura.append(cadastro['mar'])
+    temperatura.append(cadastro['abr'])
+    temperatura.append(cadastro['maio'])
+    temperatura.append(cadastro['jun'])
+    listadetemperatura.append(cadastro)
+
+lugar = input(f'Digite o nome do lugar: ').strip()
+jan = float(input('Digite a temperatura de janeiro: '))
+fev = float(input('Digite a temperatura de fevereiro: '))
+mar = float(input('Digite a temperatura de março: '))
+abr = float(input('Digite a temperatura de abril: '))
+maio = float(input('Digite a temperatura de maio: '))
+jun = float(input('Digite a temperatura de junho: '))
+
+cadastrar_temperaturas(jan,fev,mar,abr,maio,jun)
+media = sum(temperatura) / len(temperatura)
+print(media)
+print(listadetemperatura)
