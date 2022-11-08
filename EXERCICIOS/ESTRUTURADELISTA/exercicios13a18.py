86b6e2773354aa298b94c824e69514f15f8779c8
walissonsou <walissonsouza96@hotmail.com>
2022-11-08 20:42:43 -0300

    Resolução de exercícios PYTHONBRASIL

--- EXERCICIOS/ESTRUTURADELISTA/exercicios13a18.py
+++ EXERCICIOS/ESTRUTURADELISTA/exercicios13a18.py
@@ -66,3 +66,43 @@ elif (soma_respostas == 5):
  print("\nAssassino")
 
 
+# Em uma competição de salto em distância cada atleta tem direito a cinco saltos.
+# O resultado do atleta será determinado pela média dos cinco valores restantes.
+# Você deve fazer um programa que receba o nome e as cinco distâncias alcançadas pelo atleta
+# em seus saltos e depois informe o nome, os saltos e a média dos saltos. O programa
+# deve ser encerrado quando não for informado o nome do atleta .A saída do programa deve
+# ser conforme o exemplo abaixo:
+
+
+while True:
+    nome = input(
+        "Digite o nome do atleta ou enter para encerrar: "
+    )
+    if nome == "":
+        break
+
+    atleta = {
+        "nome": nome,
+        "saltos": [],
+        "media": 0,
+        "melhor_salto": 0,
+        "pior_salto": 0
+    }
+for i in range(5):
+    atleta.get("saltos").append(
+        float(input(f'Distancia primeiro salto: {i+1}'))
+    )
+    atleta.get("saltos").sort()  # ? Ordena a lista
+    atleta["pior_salto"] = atleta.get("saltos").pop(0)
+    atleta["melhor_salto"] = atleta.get("saltos").pop()
+    atleta["media"] = sum(atleta.get("saltos")) / 3
+    print(
+        f"\nMelhor salto: {atleta.get('melhor_salto'):.1f} m"
+        f"\nPior salto: {atleta.get('pior_salto'):.1f} m"
+        f"\nMédia dos demais saltos: {atleta.get('media'):.1f} m\n"
+    )
+    atletas.append(atleta)
+
+print("\n\nResultado final")
+for atleta in atletas:
+    print(f"{atleta.get('nome')}: {atleta.get('media'):.1f} m")
