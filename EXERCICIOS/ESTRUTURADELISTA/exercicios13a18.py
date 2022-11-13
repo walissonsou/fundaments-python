bcaf4a84ee928d188bac40f65cd18cae4524261a
walissonsou <walissonsouza96@hotmail.com>
2022-11-13 13:21:51 -0300

    Resolução de exercícios PYTHONBRASIL

--- EXERCICIOS/ESTRUTURADELISTA/exercicios13a18.py
+++ EXERCICIOS/ESTRUTURADELISTA/exercicios13a18.py
@@ -204,7 +204,11 @@ print('Existem {} notas abaixo da média, e elas são: {} \n'.format(len(notasab
 print('Fim')
 
 
-# Utilize uma lista para resolver o problema a seguir. Uma empresa paga seus vendedores com base em comissões. O vendedor recebe $200 por semana mais 9 por cento de suas vendas brutas daquela semana. Por exemplo, um vendedor que teve vendas brutas de $3000 em uma semana recebe $200 mais 9 por cento de $3000, ou seja, um total de $470. Escreva um programa (usando um array de contadores) que determine quantos vendedores receberam salários nos seguintes intervalos de valores:
+# Utilize uma lista para resolver o problema a seguir. Uma empresa paga seus vendedores com base em comissões.
+# O vendedor recebe $200 por semana mais 9 por cento de suas vendas brutas daquela semana.
+# Por exemplo, um vendedor que teve vendas brutas de $3000 em uma semana recebe $200 mais 9 por cento de $3000,
+# ou seja, um total de $470. Escreva um programa (usando um array de contadores) que determine
+# quantos vendedores receberam salários nos seguintes intervalos de valores:
 # $200 - $299
 # $300 - $399
 # $400 - $499
@@ -255,7 +259,38 @@ arquivoRelatorio.write('\nEspaco medio ocupado: %.2f MB' %
 arquivoRelatorio.close()
 
 
-
+# Utilize uma lista para resolver o problema a seguir. Uma empresa paga seus vendedores com base em comissões.
+# O vendedor recebe $200 por semana mais 9 por cento de suas vendas brutas daquela semana.
+# Por exemplo, um vendedor que teve vendas brutas de $3000 em uma semana recebe $200 mais 9 por cento de $3000,
+# ou seja, um total de $470. Escreva um programa (usando um array de contadores) que determine
+# quantos vendedores receberam salários nos seguintes intervalos de valores:
+l=[0,0,0,0,0,0,0,0,0]
+x=0
+while True:
+    n=float(input('Inf seu salario %d° R$: '%(x+1)))
+    if 200 < n <299:
+        l[0]+=1
+    elif 300 < n > 399:
+        l[1]+=1
+    elif 400 < n > 499:
+        l[2]+=1
+    elif 500 < n > 599:
+        l[3]+=1
+    elif 600 < n > 699:
+        l[4]+=1
+    elif 700 < n > 799:
+        l[5]+=1
+    elif 800 < n > 899:
+        l[6]+=1
+    elif 900 < n > 999:
+        l[7]+=1
+    elif n >= 1000:
+        l[8]=[8]+1
+    x+=1
+    d = input('deseja continuar?(s ou n)')
+    if d == 'n':
+        break
+print(l[0])
 
 
 
